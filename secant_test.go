eac0464bcb984027ca382e0f0ce8105acdcdd34d
Kraus1049 <kraus1049@gmail.com>
2016-02-22 11:30:29 +0900

    done secant.go and secant_test.go

--- secant_test.go
+++ secant_test.go
@@ -1,6 +1,7 @@
 package k3library
 
 import (
+	"errors"
 	"math"
 	"testing"
 )
@@ -9,47 +10,48 @@ type secantTest struct {
 	f                    func(float64) float64
 	start, end, num, eps float64
 	expected             float64
-	ok                   bool
+	err                  error
 }
 
 func TestSecant(t *testing.T) {
-	// f, start, end, num, eps, expected ,ok
+	// f, start, end, num, eps, expected ,err
 	testsecant := []secantTest{
 		{func(x float64) float64 { return x },
-			-1, 1, 0, 1e-15, 0, true},
+			-1, 1, 0, 1e-15, 0, nil},
 		{func(x float64) float64 { return math.Sin(x) },
-			3, 4, 0, 1e-15, math.Pi, true},
+			3, 4, 0, 1e-15, math.Pi, nil},
 		{func(x float64) float64 { return math.Log(x) },
-			5, 1, 1, 1e-15, math.E, true},
+			5, 1, 1, 1e-15, math.E, nil},
 	}
 
 	for i := range testsecant {
 		test := &testsecant[i]
 
-		actual, ok := Secant(test.start, test.end, test.f, test.num, test.eps)
+		actual, err := Secant(test.start, test.end, test.f, test.num, test.eps)
 
 		if !Epsequal(actual, test.expected, test.eps) {
 			t.Errorf("actual = %v,expected = %v", actual, test.expected)
-		} else if ok != test.ok {
-			t.Errorf("actual = %v,expected = %v", ok, test.ok)
+		} else if err != test.err {
+			t.Errorf("actual = %v,expected = %v", err, test.err)
 		}
 	}
 }
 
 func testSecantinvalidargument(t *testing.T) {
-	// f, start, end, num, eps, expected ,ok
+	// f, start, end, num, eps, expected ,err
 	testsecantinvalidargument := []secantTest{
 		{func(x float64) float64 { return x },
-			1, 1, 0, 1e-15, 0, false},
+			1, 1, 0, 1e-15, 0,
+			errors.New("Secant:Invalid argument")},
 	}
 
 	for i := range testsecantinvalidargument {
 		test := &testsecantinvalidargument[i]
 
-		_, ok := Secant(test.start, test.end, test.f, test.num, test.eps)
+		_, err := Secant(test.start, test.end, test.f, test.num, test.eps)
 
-		if ok != test.ok {
-			t.Errorf("actual = %v,expected = %v", ok, test.ok)
+		if err.Error() != test.err.Error() {
+			t.Errorf("actual = %v,expected = %v", err, test.err)
 		}
 	}
 }

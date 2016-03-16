a29b74c9c49368be7d2473b2a9fdf05b353d8871
Kraus1049 <kraus1049@gmail.com>
2016-03-16 12:08:17 +0900

    define new error ErrCannotSolve

--- err_struct.go
+++ err_struct.go
@@ -8,4 +8,5 @@ var (
 	ErrInvalid      = errors.New("Invalid argument")
 	ErrInfiniteLoop = errors.New("Infinite loop")
 	ErrOutOfRange   = errors.New("Out of range")
+	ErrCannotSolve  = errors.New("Cannot solve")
 )

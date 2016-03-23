9c04271c0fbb70b145a91ccc6173ca70f79ec93d
Kraus1049 <kraus1049@gmail.com>
2016-03-23 19:18:46 +0900

    wrote trapezoid.go and test

---
+++ trapezoid.go
@@ -0,0 +1,50 @@
+package k3library
+
+func Trapezoid(f func(float64) float64, a, b, eps float64) (float64, error) {
+	if a > b {
+		return -1, ErrInvalid
+	}
+
+	n := 1
+	pre_ans := trapezoid1(f, a, b, &n)
+	ans := trapezoid(f, a, b, &n, pre_ans)
+
+	cnt := 0
+
+	for !EpsEqual(pre_ans, ans, eps) {
+		pre_ans = ans
+		ans = trapezoid(f, a, b, &n, pre_ans)
+
+		if cnt++; cnt >= 100 {
+			break
+		}
+	}
+
+	return ans, nil
+}
+
+func trapezoid1(f func(float64) float64, a, b float64, n *int) float64 {
+	h := float64(b-a) / float64(*n)
+
+	sum := 0.0
+
+	for i := 1; i < *n; i++ {
+		sum += f(a + float64(i)*h)
+	}
+
+	*n *= 2
+	return sum * h / 2.0
+}
+
+func trapezoid(f func(float64) float64, a, b float64, n *int, pre_ans float64) float64 {
+	*n *= 2
+	h := float64(b-a) / float64(*n)
+
+	sum := 0.0
+	for i := 1; i < *n; i += 2 {
+		sum += f(a + float64(i)*h)
+	}
+
+	return pre_ans/2.0 + h*sum
+
+}
---
+++ trapezoid_test.go
@@ -0,0 +1,56 @@
+package k3library_test
+
+import (
+	. "github.com/kraus1049/k3library"
+	"math"
+	"testing"
+)
+
+type trapezoidTest struct {
+	f                   func(float64) float64
+	a, b, eps, expected float64
+	err                 error
+}
+
+func TestTrapezoid(t *testing.T) {
+	var testTrapezoid = []trapezoidTest{
+		{func(x float64) float64 { return x },
+			0, 10, 1e-6, 50,
+			nil},
+		{func(x float64) float64 { return 10 },
+			5, 10, 1e-6, 50,
+			nil},
+		{func(x float64) float64 { return math.Sin(x) },
+			0, math.Pi / 2, 1e-6, 1,
+			nil},
+	}
+
+	for i := range testTrapezoid {
+		test := &testTrapezoid[i]
+		actual, err := Trapezoid(test.f, test.a, test.b, test.eps)
+
+		if err != test.err {
+			t.Errorf("%v: actual = %v, expected = %v\n", i, err, test.err)
+		} else if !EpsEqual(actual, test.expected, test.eps) {
+			t.Errorf("%v: actual = %v, expected = %v\n", i, actual, test.expected)
+		}
+	}
+}
+
+func TestTrapezoidInvalidArgument(t *testing.T) {
+	var testTrapezoidInvalidArgument = []trapezoidTest{
+		{func(x float64) float64 { return x },
+			10, 0, 1e-6, 50,
+			ErrInvalid},
+	}
+
+	for i := range testTrapezoidInvalidArgument {
+		test := &testTrapezoidInvalidArgument[i]
+		_, err := Trapezoid(test.f, test.a, test.b, test.eps)
+
+		if err != test.err {
+			t.Errorf("%v: actual = %v, expected = %v\n", i, err, test.err)
+		}
+	}
+
+}

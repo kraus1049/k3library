2668c25ca3d43bc24c697e23a8daed8928b46102
Kraus1049 <kraus1049@gmail.com>
2016-03-30 11:16:24 +0900

    wrote Pro() and test

--- calc_vec_mat.go
+++ calc_vec_mat.go
@@ -98,3 +98,147 @@ func Sum(xs ...interface{}) (interface{}, error) {
 	}
 	return nil, ErrInvalid
 }
+
+func Pro(xs ...interface{}) (interface{}, error) {
+	if len(xs) == 1 {
+		if x, ok := xs[0].(float64); ok {
+			return x, nil
+		} else if v, ok := xs[0].(Vec); ok {
+			return v, nil
+		} else if m, ok := xs[0].(Mat); ok {
+			return m, nil
+		} else {
+			return nil, ErrInvalid
+		}
+	} else if len(xs) >= 2 {
+		switch xs[0].(type) {
+		case float64:
+			x, _ := xs[0].(float64)
+
+			switch xs[1].(type) {
+			case float64:
+				y, _ := xs[1].(float64)
+				xs = append([]interface{}{x * y}, xs[2:]...)
+			case Vec:
+				y, _ := xs[1].(Vec)
+				pro := y.Copy()
+				for i := 0; i < pro.Row; i++ {
+					tmp := pro.At(i) * x
+					pro.Write(i, tmp)
+				}
+				xs = append([]interface{}{pro}, xs[2:]...)
+			case Mat:
+				y, _ := xs[1].(Mat)
+				pro := y.Copy()
+				for i := 0; i < pro.Col; i++ {
+					for j := 0; j < pro.Row; j++ {
+						tmp := pro.At(i, j) * x
+						pro.Write(i, j, tmp)
+					}
+				}
+				xs = append([]interface{}{pro}, xs[2:]...)
+			default:
+				return nil, ErrInvalid
+			}
+		case Vec:
+			x, _ := xs[0].(Vec)
+
+			switch xs[1].(type) {
+			case float64:
+				y, _ := xs[1].(float64)
+				pro := x.Copy()
+				for i := 0; i < pro.Row; i++ {
+					tmp := pro.At(i) * y
+					pro.Write(i, tmp)
+				}
+				xs = append([]interface{}{pro}, xs[2:]...)
+
+			case Vec:
+				y, _ := xs[1].(Vec)
+				if x.Row != y.Row {
+					return nil, ErrInvalid
+				}
+
+				sum := 0.0
+				for i := 0; i < x.Row; i++ {
+					sum += x.At(i) * y.At(i)
+				}
+				xs = append([]interface{}{sum}, xs[2:]...)
+			case Mat:
+				y, _ := xs[1].(Mat)
+				if y.Col != 1 {
+					return nil, ErrInvalid
+				}
+
+				m := NewMat(x.Row, y.Row)
+				for i := 0; i < x.Row; i++ {
+					for j := 0; j < y.Row; j++ {
+						sum := 0.0
+						for k := 0; k < y.Col; k++ {
+							sum += x.At(i) * y.At(k, j)
+						}
+						m.Write(i, j, sum)
+					}
+				}
+				xs = append([]interface{}{m}, xs[2:]...)
+			default:
+				return nil, ErrInvalid
+			}
+		case Mat:
+			x, _ := xs[0].(Mat)
+			switch xs[1].(type) {
+			case float64:
+				y, _ := xs[1].(float64)
+				pro := x.Copy()
+				for i := 0; i < pro.Col; i++ {
+					for j := 0; j < pro.Row; j++ {
+						tmp := pro.At(i, j) * y
+						pro.Write(i, j, tmp)
+					}
+				}
+				xs = append([]interface{}{pro}, xs[2:]...)
+
+			case Vec:
+				y, _ := xs[1].(Vec)
+				pro := NewVec(x.Col)
+				for i := 0; i < x.Col; i++ {
+					sum := 0.0
+					for k := 0; k < y.Row; k++ {
+						sum += x.At(i, k) * y.At(k)
+					}
+					pro.Write(i, sum)
+				}
+				xs = append([]interface{}{pro}, xs[2:]...)
+			case Mat:
+				y, _ := xs[1].(Mat)
+
+				if x.Row != y.Col {
+					return nil, ErrInvalid
+				}
+				pro := NewMat(x.Col, y.Row)
+
+				for i := 0; i < x.Col; i++ {
+					for j := 0; j < y.Row; j++ {
+						sum := 0.0
+						for k := 0; k < x.Row; k++ {
+							sum += x.At(i, k) * y.At(k, j)
+						}
+						pro.Write(i, j, sum)
+					}
+				}
+				xs = append([]interface{}{pro}, xs[2:]...)
+			default:
+				return nil, ErrInvalid
+			}
+
+		default:
+			return nil, ErrInvalid
+		}
+
+		ans, err := Pro(xs...)
+		return ans, err
+	}
+
+	return nil, ErrInvalid
+
+}
--- calc_vec_mat_test.go
+++ calc_vec_mat_test.go
@@ -24,6 +24,12 @@ type sumTest struct {
 	err      error
 }
 
+type proTest struct {
+	xs       []interface{}
+	expected interface{}
+	err      error
+}
+
 func TestSum(t *testing.T) {
 	v1 := NewVecSet([]float64{1, 2})
 	v2 := NewVecSet([]float64{2, 4})
@@ -98,6 +104,69 @@ func TestSum(t *testing.T) {
 		}
 
 	}
+}
+
+func TestPro(t *testing.T) {
+	x1 := 2.0
+	x2 := 4.0
+	x3 := 8.0
+	x4 := 28.0
+	x5 := 17640.0
+	v1 := NewVecSet([]float64{1, 2, 3})
+	v2 := NewVecSet([]float64{2, 4, 6})
+	v3 := NewVecSet([]float64{8, 16, 24})
+	v5 := NewVecSet([]float64{14, 32, 50})
+	v6 := NewVecSet([]float64{1, 2})
+	m1 := NewMatSet([][]float64{{1, 2, 3}, {4, 5, 6}})
+	m2 := NewMatSet([][]float64{{2, 4, 6}, {8, 10, 12}})
+	m3 := NewMatSet([][]float64{{1, 2, 3}})
+	m4 := NewMatSet([][]float64{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}})
+	m5 := NewMatSet([][]float64{{30, 36, 42}, {66, 81, 96}, {102, 126, 150}})
+	m6 := NewMatSet([][]float64{{1, 2, 3}, {2, 4, 6}, {3, 6, 9}})
+
+	var testPro = []proTest{
+		{[]interface{}{x1, x1}, x2, nil},
+		{[]interface{}{x1, x1, x1}, x3, nil},
+		{[]interface{}{x1, v1}, v2, nil},
+		{[]interface{}{x1, x1, v2}, v3, nil},
+		{[]interface{}{x1, m1}, m2, nil},
+		{[]interface{}{v1, x1}, v2, nil},
+		{[]interface{}{v1, v2}, x4, nil},
+		{[]interface{}{v1, m3}, m6, nil},
+		{[]interface{}{m1, x1}, m2, nil},
+		{[]interface{}{m4, v1}, v5, nil},
+		{[]interface{}{m4, m4}, m5, nil},
+		{[]interface{}{v1, v1, m1, m4, v1, v6},
+			x5, nil},
+	}
+
+	for i := range testPro {
+		test := &testPro[i]
+		actual, err := Pro(test.xs...)
+
+		switch actual.(type) {
+		case float64:
+			if actual != test.expected {
+				t.Errorf("%v: actual = %v,expected = %v\n", i, actual, test.expected)
+			} else if err != test.err {
+				t.Errorf("%v: actual = %v,expected = %v\n", i, actual, test.expected)
+			}
+
+		case Vec, Mat:
+			if !reflect.DeepEqual(actual, test.expected) {
+				t.Errorf("%v: actual = %v,expected = %v\n", i, actual, test.expected)
+			} else if err != test.err {
+				t.Errorf("%v: actual = %v,expected = %v\n", i, actual, test.expected)
+			}
+
+		case nil:
+			if err != test.err {
+				t.Errorf("%v: actual = %v,expected = %v\n", i, actual, test.expected)
+			}
+		}
+
+	}
+
 }
 
 func TestVSub(t *testing.T) {

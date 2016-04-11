6ce0f9093fbf1f78fc90efdec3495185c7319a87
Kraus1049 <kraus1049@gmail.com>
2016-04-11 16:51:20 +0900

    Add nub() in Csv2func()

--- csv2func.go
+++ csv2func.go
@@ -2,28 +2,12 @@ package k3library
 
 func Csv2func(filepath string, parser func(string) (map[string]string, [][2]float64, error)) (func(float64) (float64, error), error) {
 
-	bin_search := func(xys [][2]float64, x float64) (float64, error) {
-		low, high := 0, len(xys)-1
-
-		var ix int
-		for low <= high {
-			ix = (low + high) / 2
-
-			if x == xys[ix][0] {
-				return xys[ix][1], nil
-			} else if x < xys[ix][0] {
-				high = ix - 1
-			} else {
-				low = ix + 1
-			}
-		}
-		return xys[ix][1], nil
-	}
-
 	_, xys, err := parser(filepath)
 	if err != nil {
 		return nil, err
 	}
+
+	xys = nub(xys)
 
 	x_min, x_max := xys[0][0], xys[len(xys)-1][0]
 
@@ -36,8 +20,41 @@ func Csv2func(filepath string, parser func(string) (map[string]string, [][2]floa
 			return xys[0][1], nil
 		}
 
-		return bin_search(xys, x)
+		return binSearch(xys, x)
 	}
 
 	return f, nil
+}
+
+func binSearch(xys [][2]float64, x float64) (float64, error) {
+	low, high := 0, len(xys)-1
+
+	var ix int
+	for low <= high {
+		ix = (low + high) / 2
+
+		if x == xys[ix][0] {
+			return xys[ix][1], nil
+		} else if x < xys[ix][0] {
+			high = ix - 1
+		} else {
+			low = ix + 1
+		}
+	}
+	return xys[ix-1][1], nil
+}
+
+func nub(x [][2]float64) [][2]float64 {
+	ans := make([][2]float64, 0)
+	ans = append(ans, x[0])
+
+	for i := 1; i < len(x)-2; i++ {
+		if x[i][1] != x[i-1][1] {
+			ans = append(ans, x[i])
+		}
+	}
+
+	ans = append(ans, x[len(x)-1])
+
+	return ans
 }
